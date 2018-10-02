9a1d1a0aad4ea94ea82800ba1de93845f5724412
illuzen <illuzen@users.noreply.github.com>
2018-10-02 13:27:56 -0700

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,4 +1,15 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  
+  networks: {
+    'dev': {
+      host: 'localhost',
+      port: 8545,
+      network_id: "424242",
+      before_timeout: 300,
+      test_timeout: 300
+   }
+  }
+}
 };
